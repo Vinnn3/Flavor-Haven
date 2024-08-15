9a535f1fec5827cd5bb5b601037891ea4e8fa499
farhiya21 <fbhy21@gmail.com>
2024-08-15 11:17:40 +0300

    styling

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,19 +3,37 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div className='nav-bar'>
-      <h1>Flavour Haven</h1>
-      <NavLink to={'/'}>
-      <p>Home</p>
-      </NavLink>
-      <NavLink to={'/create-recipe'}>
-        <p>Create Recipe</p>
-      </NavLink>
-      <NavLink to={'/recipe/:id'}>
-        <p>Recipes</p>
-      </NavLink>
-
-</div>
+    <nav className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Flavour Haven</h1>
+        <div className="space-x-4">
+          <NavLink 
+            to={'/'} 
+            className={({ isActive }) => 
+              `text-lg ${isActive ? 'text-blue-400' : 'text-white'} hover:text-blue-300`
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink 
+            to={'/create-recipe'} 
+            className={({ isActive }) => 
+              `text-lg ${isActive ? 'text-blue-400' : 'text-white'} hover:text-blue-300`
+            }
+          >
+            Create Recipe
+          </NavLink>
+          <NavLink 
+            to={'/recipes'} 
+            className={({ isActive }) => 
+              `text-lg ${isActive ? 'text-blue-400' : 'text-white'} hover:text-blue-300`
+            }
+          >
+            Recipes
+          </NavLink>
+        </div>
+      </div>
+    </nav>
   );
 };
 
